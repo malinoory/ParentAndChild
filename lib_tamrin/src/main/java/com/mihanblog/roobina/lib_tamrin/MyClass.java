5fec39de98981bf90a1fbd4d5417e3ac78fd3106
mahdi alinouri <m.alinoory@gmail.com>
2018-11-17 00:02:26 +0330

    parent and child class

---
+++ lib_tamrin/src/main/java/com/mihanblog/roobina/lib_tamrin/MyClass.java
@@ -0,0 +1,9 @@
+package com.mihanblog.roobina.lib_tamrin;
+
+public class MyClass {
+    public static void main( String[] args ) {
+    }
+        public static void printName() {
+            System.out.print("mahdialinoori");
+    }
+        }
---
+++ lib_tamrin/src/main/java/com/mihanblog/roobina/lib_tamrin/child.java
@@ -0,0 +1,15 @@
+package com.mihanblog.roobina.lib_tamrin;
+
+public class child extends MyClass  {
+
+ public static void main(String[] args) {
+  printName ();
+
+
+ }
+
+}
+
+
+
+
